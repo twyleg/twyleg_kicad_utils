4d7c675487d578d80768089783bacf7797737eb0
Torsten Wylegala <mail@twyleg.de>
2023-12-21 13:44:11 +0100

    Fixed issue with absolute paths for output directory when output dir is not a subdir of the working dir

--- twyleg_kicad_utils/build.py
+++ twyleg_kicad_utils/build.py
@@ -8,9 +8,14 @@ from twyleg_kicad_utils.process import run
 
 def build(working_dir: Path, output_dir: Path, targets: List[str]):
 
-    run(f"podman run \
-        -v {working_dir}/:/root/project/ \
-        -w /root/project \
-        ghcr.io/inti-cmnb/kicad7_auto_full:latest \
-        kibot -d {output_dir}".split() + targets
-    )
+    logging.info("Output directory: %s", output_dir)
+
+    if working_dir in output_dir.parents:
+        run(f"podman run \
+            -v {working_dir}/:/root/project/ \
+            -w /root/project \
+            ghcr.io/inti-cmnb/kicad7_auto_full:latest \
+            kibot -d {output_dir.relative_to(working_dir)}".split() + targets
+        )
+    else:
+        logging.error("Output dir outside of working dir. Unable to access output dir in container.")
